1c07771616de82546878d896d6d8f76734bd0d7f
Brian Hurt <bhurt42@gmail.com>
2022-04-06 08:10:54 -0500

    Re-maybe-ify write ducts again.
    This allows us to share code between read and write duct endpoints.

---
+++ src/Data/Conduit/Parallel/Internal/Duct/Utils.hs
@@ -0,0 +1,67 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+-- |
+-- Module      : Data.Conduit.Parallel.Internal.Duct.Utils
+-- Description : Miscellaneous usefull stuff for Ducts.
+-- Copyright   : (c) Brian Hurt, 2022
+-- License     : BSD 3-clause
+-- Maintainer  : bhurt42@gmail.com
+-- Stability   : experimental
+--
+-- This is an internal module of the Parallel Conduits.  You almost
+-- certainly want to use [Data.Conduit.Parallel](Data-Conduit-Parallel.html)
+-- instead.  Anything -- in this module not explicitly re-exported 
+-- by [Data.Conduit.Parallel](Data-Conduit-Parallel.html)
+-- is for internal use only, and will change or disappear without
+-- notice.  Use at your own risk.
+--
+module Data.Conduit.Parallel.Internal.Duct.Utils where
+
+    import           Control.Concurrent.STM
+    import           Control.Exception
+    import           Control.Monad.IO.Class
+
+    -- | Exception to throw when a closed duct is encountered.
+    --
+    -- It is common  when combining ducts to want to roll back changes
+    -- to previous ducts when we encounter a closed duct.  We can
+    -- do this by wrapping all the changes with a @catchSTM@, and then
+    -- doing a @throwSTM@ when we encounter a closed duct.
+    --
+    -- This is the value to throw to do this.
+    data ClosedDuctException = ClosedDuctException
+        deriving (Show)
+
+    instance Exception ClosedDuctException
+
+
+    -- | Convert a retry into a Nothing.
+    --
+    -- Another common thing to want to do is to catch when a STM action
+    -- does a retry.  This function returns @Nothing@ when the STM action
+    -- retries, and @Just a@ when it does not.
+    catchRetry :: forall a .  STM a -> STM (Maybe a)
+    catchRetry act = (Just <$> act) `orElse` pure Nothing
+
+
+    checkClosed :: forall a b m .
+                            MonadIO m
+                            => ((STM (Maybe a) -> STM (Maybe a)) -> b)
+                            -> m b
+    checkClosed makeB = do
+        tvar <- liftIO $ newTVarIO True
+        let f :: STM (Maybe a) -> STM (Maybe a)
+            f act = do
+                s <- readTVar tvar
+                case s of
+                    False -> pure Nothing
+                    True  -> do
+                        r :: Maybe a <- act
+                        case r of
+                            Just a  -> pure $ Just a
+                            Nothing -> do
+                                writeTVar tvar False
+                                pure Nothing
+        pure $ makeB f
+
+
