8c61b243486a30700fdffb803d2a4dc1fde4f9e0
Brian Hurt <bhurt42@gmail.com>
2022-04-17 10:11:54 -0500

    Document the Duct/No module.

--- src/Data/Conduit/Parallel/Internal/Duct/No.hs
+++ src/Data/Conduit/Parallel/Internal/Duct/No.hs
@@ -15,6 +15,12 @@
 -- is for internal use only, and will change or disappear without
 -- notice.  Use at your own risk.
 --
+-- = Purpose
+--
+-- Read and write endpoints for ducts that are already closed.  Useful for
+-- implementing parallel conduits that should not be read and/or write
+-- values, i.e. sources and sinks.
+--
 module Data.Conduit.Parallel.Internal.Duct.No where
 
     import           Data.Void
@@ -22,9 +28,22 @@ module Data.Conduit.Parallel.Internal.Duct.No where
     import           Data.Conduit.Parallel.Internal.Duct
 
 
+    -- | Pre-closed read conduit
+    --
+    -- This is a read endpoint for a duct that is already closed.  This
+    -- is useful for situations where a parallel conduit should not read
+    -- any values (i.e. it is a source).  It's special cased because
+    -- it can be more efficiently implemented.
     noRead :: ReadDuct Simple m ()
     noRead = ReadDuct $ pure (pure Nothing)
 
+    -- | Pre-closed write conduit.
+    --
+    -- This is a write endpoint for a duct that is already closed.  This
+    -- is useful for situaitons where a parallel conduit should not write
+    -- any values (i.e. it is a sink).  It's special cased because it can
+    -- be more efficiently implemented.
+    --
     noWrite :: WriteDuct Simple m Void
     noWrite = WriteDuct $ pure absurd
 
