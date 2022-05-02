128452954b058dd33a97cb48e37abf3fdcba3de9
Brian Hurt <bhurt42@gmail.com>
2022-05-02 18:56:09 -0500

    Update ParArrow type classes.

--- src/Data/Conduit/Parallel/Internal/Arrow/Type.hs
+++ src/Data/Conduit/Parallel/Internal/Arrow/Type.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs        #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- |
@@ -22,19 +23,20 @@
 -- Provide the main @ParArrow@ type, and associated type classes.
 module Data.Conduit.Parallel.Internal.Arrow.Type where
 
+    import qualified Control.Category                     as Cat
+    import           Control.Monad.IO.Unlift
     import           Data.Functor.Contravariant
+    import           Data.Functor.Contravariant.Divisible
     import           Data.Profunctor
+    import           Data.These
 
-    -- import qualified Control.Category           as Cat
-    -- import           Control.Monad.IO.Unlift
-    -- import qualified Control.Selective          as Sel
+    -- import qualified Control.Selective                    as Sel
 
     import           Data.Conduit.Parallel.Internal.Conduit.Type
+    import           Data.Conduit.Parallel.Internal.Copy
     import           Data.Conduit.Parallel.Internal.Duct
     import           Data.Conduit.Parallel.Internal.Spawn
-
-    -- import           Data.Conduit.Parallel.Internal.Copy
-    -- import           Data.Conduit.Parallel.Internal.Utils
+    import           Data.Conduit.Parallel.Internal.Utils
 
     -- | A Parallel Arrow
     newtype ParArrow m a b =
@@ -60,37 +62,51 @@ module Data.Conduit.Parallel.Internal.Arrow.Type where
         rmap g pc = ParArrow $
                         \rd wd -> getParArrow pc rd (contramap g wd)
 
-    {-
     instance MonadUnliftIO m => Applicative (ParArrow m a) where
         pure a = ParArrow $ \rd wd -> do
                                 m1 <- spawn $ discardThread rd
-                                m2 <- spawn $ spawnThread wd
+                                m2 <- spawn $ spamThread wd a
                                 pure $ m1 >> m2
         a1 <*> a2 = ParArrow $ splice
                                 (\() () -> ()) 
-                                writeDuplicate
-                                (readBoth ($))
+                                (divide (\a -> (a,a)))
+                                (<*>)
                                 (getParArrow a1)
                                 (getParArrow a2)
 
     instance MonadUnliftIO m => Choice (ParArrow m) where
-        left' pa = ParArrow $ bypass route (getParArrow pa)
+        left' :: forall a b c .
+                    ParArrow m a b
+                    -> ParArrow m (Either a c) (Either b c)
+        left' pa = ParArrow $ bypass pre post (getParArrow pa)
             where
-                route (Right c) = Left (Right c)
-                route (Left a) = Right (a, Left)
-        right' pa = ParArrow $ bypass route (getParArrow pa)
-            where
-                route (Left c)  = Left (Left c)
-                route (Right a) = Right (a, Right)
-        
+                pre :: Either a c -> These c a
+                pre (Left a) = That a
+                pre (Right c) = This c
 
+                post :: These c b -> Either b c
+                post (This c) = Right c
+                post (That b) = Left b
+                post _        = error "Unreachable code reached!"
+
+        right' :: forall a b c .
+                    ParArrow m a b
+                    -> ParArrow m (Either c a) (Either c b)
+        right' pa = ParArrow $ bypass pre post (getParArrow pa)
+            where
+                pre :: Either c a -> These c a
+                pre (Left c) = This c
+                pre (Right a) = That a
+
+                post :: These c b -> Either c b
+                post (This c) = Left c
+                post (That b) = Right b
+                post _        = error "Unreachable code reached!"
+        
     instance MonadUnliftIO m => Cat.Category (ParArrow m) where
         id = ParArrow $ \rd wr -> spawn (copyThread rd wr)
         pc1 . pc2 = ParArrow $ fuseInternal
                                     mappend
                                     (getParArrow pc2)
                                     (getParArrow pc1)
-
-    -}
-
 
